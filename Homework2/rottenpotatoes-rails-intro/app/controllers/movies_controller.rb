ec93a4ccbac578de38be817425be1759756ce142
David Cross <davidpcross@tamu.edu>
2018-09-22 16:36:17 -0500

    Added solution for part 2, forgot to commit so some code for part 3 is in there

--- Homework2/rottenpotatoes-rails-intro/app/controllers/movies_controller.rb
+++ Homework2/rottenpotatoes-rails-intro/app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController
-# Code for this file based on this video: https://www.youtube.com/watch?v=xyi4YnX7LUQ
+# Code for this file based on these videos: https://www.youtube.com/watch?v=xyi4YnX7LUQ
+#https://www.youtube.com/watch?v=OUCQ1VJ2Lzg
+#https://www.youtube.com/watch?v=QeuYemlTu8I
 
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
@@ -12,8 +14,26 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.order(params[:sort_by])
+    @all_ratings = Movie.all_ratings
     @column_sort_by = params[:sort_by]
+    @set_ratings = params[:ratings]
+    
+    if @set_ratings and @column_sort_by
+      @movies = Movie.where(:rating => params[:ratings].keys).order(params[:sort_by])
+    elsif @set_ratings
+      @movies = Movie.where(:rating => params[:ratings].keys)
+    elsif @column_sort_by
+      @movies = Movie.order(params[:sort_by])
+    else
+      @movie = Movie.all
+    end
+    if !@set_ratings
+      @set_ratings = Hash.new
+    end
+    @movies = Movie.all
+    if params[:ratings]
+      @movies = Movie.where(:rating => params[:ratings].keys).order(params[:sort_by])
+    end
   end
 
   def new
--- Homework2/rottenpotatoes-rails-intro/app/models/movie.rb
+++ Homework2/rottenpotatoes-rails-intro/app/models/movie.rb
@@ -1,2 +1,8 @@
+# Code for this file based on this video: https://www.youtube.com/watch?v=OUCQ1VJ2Lzg
 class Movie < ActiveRecord::Base
+    def self.all_ratings
+        a = Array.new
+        self.select("rating").uniq.each {|x| a.push(x.rating)}
+        a.sort.uniq
+    end
 end
